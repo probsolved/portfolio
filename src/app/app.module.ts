1fcd1f7ccb1dc1642388765a8b3cf64ed9905194
PRob-Solved <47791675+PRob-Solved@users.noreply.github.com>
2022-03-25 12:09:36 -0500

    New contact component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,12 @@ import { AboutComponent } from './about/about.component';
 import { EducationComponent } from './education/education.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { CompetencesComponent } from './competences/competences.component';
-import { ContactComponent } from './contact/contact.component';
 import { HeaderComponent } from './header/header.component';
 import { ReferencesComponent } from './references/references.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { ContactmeComponent } from './contactme/contactme.component';
 
 
 @NgModule({
@@ -21,10 +22,11 @@ import { HomeComponent } from './home/home.component';
     EducationComponent,
     ProjectsComponent,
     CompetencesComponent,
-    ContactComponent,
     HeaderComponent,
     ReferencesComponent,
-    HomeComponent
+    HomeComponent,
+    ContactComponent,
+    ContactmeComponent
   ],
   imports: [
     BrowserModule,
